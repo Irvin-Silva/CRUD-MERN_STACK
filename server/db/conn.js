37000ae8f4e40f46fbb9077d37f7bbd960d0291a
IrvinSilva <64101694+sabi-tech@users.noreply.github.com>
2023-02-17 12:28:59 -0500

    Cambio texto servidor

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,4 +4,4 @@ const DB = "mongodb+srv://irvin:1234@cluster0.47xi8nh.mongodb.net/mernstack?retr
 mongoose.connect(DB,{
     useNewUrlParser:true,
     useUnifiedTopology:true
-}).then(()=> console.log("La conexciado el servidor")).catch((error)=> console.log(error.message));
+}).then(()=> console.log("Se ha establecido la conexión con el servidor")).catch((error)=> console.log(error.message));
